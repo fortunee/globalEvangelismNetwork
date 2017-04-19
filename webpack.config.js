c0c652227102f31c88fd74893270501173a1a9c5
Fortune Ekeruo <legalmody@gmail.com>
2017-04-19 16:21:55 +0100

    Add a webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'cheap-module-eval-source-map',
+  noInfo: false,
+  entry: [
+    'eventsource-polyfill', // necessary for IE hot reloading
+    'webpack-hot-middleware/client?reload=true', // the query parameter reloads the page if hot reload fails for some reason
+    './src/index' // it is import that the src/index file is last in the array as order here is critical and important.
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + '/dist', // The physical files are only output for production builds by running `npm run build`.
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './src'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'] },
+      {test: /(\.css)$/, loaders: ['style', 'css'] },
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
+};
