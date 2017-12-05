3972762a7d6516da12c9b113497ecb3420c66c00
Fortune Ekeruo <legalmody@gmail.com>
2017-12-05 17:39:23 +0100

    Saves user in db

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
 import validateData from './../validations/signup.validator';
+
+import User from './../models/user';
 
 let router = express.Router();
 
@@ -7,7 +10,14 @@ router.post('/', (req, res) => {
     const { errors, isValid } = validateData(req.body);
 
     if(isValid) {
-        res.status(200).json({ success: true });
+        const { name, username, email, password }  = req.body;
+        const password_digest = bcrypt.hashSync(password, 10);
+
+        User.forge({
+            name, username, email, password_digest
+        }, { hasTimestamps: true }).save()
+            .then(user => res.status(200).json({ success: true }))
+            .catch(err => res.status(500).json({ error: err }));
     } else {
         res.status(400).json(errors);
     }
