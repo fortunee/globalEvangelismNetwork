1d80b3858e31450e52511cda67cb73775b34073b
Fortune Ekeruo <legalmody@gmail.com>
2018-01-25 00:37:12 +0100

    Add a QuestionForm component

---
+++ client/components/questions/QuestionForm.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import TextFieldGroup from '../shared/TextFieldGroup';
+
+class QuestionForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            question: '',
+            isLoading: false
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+    
+    onChange(e) {
+        e.preventDefault();
+        const field = e.target.name;
+        const value = e.target.value;
+
+        this.setState({[field]: value});
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        // this.setState({isLoading: true});
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <textarea 
+                    className="form-control" 
+                    name="question" 
+                    value={this.state.question}
+                    onChange={this.onChange}
+                    rows="2">
+                </textarea>
+
+                <br/>
+                <div className="form-group">
+                    <button 
+                        disabled={this.state.isLoading} 
+                        className="btn btn-lg btn-success pull-right" 
+                        type="submit">
+                        Ask a question
+                    </button>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default QuestionForm;
--- client/components/questions/Questions.js
+++ client/components/questions/Questions.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import QuestionForm from './QuestionForm';
 
 class Questions extends Component {
     render() {
         return (
             <div>
                 <h1>Questions</h1>
+                <QuestionForm />
             </div>
         );
     }
